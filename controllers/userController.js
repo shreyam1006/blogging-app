0071f2a8b501288101204117b0d6d35a413b24e7
Shreya Maheshwari <shreyam@gluelabs.com>
2023-07-05 14:12:30 +0530

    ignore node_modules

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,16 +60,11 @@ const login = async (req, res) => {
     if (user) {
       const isSame = await bcrypt.compare(password, user.password);
 
-      //if password is the same
-      //generate token with the user's id and the secretKey in the env file
-
       if (isSame) {
         let token = jwt.sign({ id: user.id }, process.env.secretKey, {
           expiresIn: 1 * 24 * 60 * 60 * 1000,
         });
 
-        //if password matches wit the one in the database
-        //go ahead and generate a cookie for the user
         res.cookie("auth-token", token, {
           maxAge: 1 * 24 * 60 * 60,
           httpOnly: true,
