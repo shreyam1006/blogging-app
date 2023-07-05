e6c73780341f35912cd2e5f593c3cfe40f3f0bf6
Shreya Maheshwari <shreyam@gluelabs.com>
2023-07-05 11:15:30 +0530

    login signup flow

---
+++ models/index.js
@@ -0,0 +1,30 @@
+//importing modules
+const { Sequelize, DataTypes } = require("sequelize");
+
+//Database connection with dialect of postgres specifying the database we are using
+//port for my database is 5433
+//database name is discover
+const sequelize = new Sequelize("blogging-app", "postgres", "postgres", {
+  host: "localhost",
+  dialect: "postgres",
+});
+
+//checking if connection is done
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(`Database connected to discover`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+//connecting to model
+db.users = require("./userModel")(sequelize, DataTypes);
+
+//exporting the module
+module.exports = db;
