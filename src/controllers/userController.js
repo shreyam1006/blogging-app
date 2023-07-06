255508fc3856e9132655cabc740ccd21b6a539fb
Shreya Maheshwari <shreyam@gluelabs.com>
2023-07-06 13:10:55 +0530

    fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,6 @@ const bcrypt = require("bcrypt");
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-// Assigning users to the variable User
 const User = db.users;
 
 //signing a user up
@@ -15,12 +14,12 @@ const signup = async (req, res) => {
       email,
       password: await bcrypt.hash(password, 10),
     };
-    //saving the user
+
     const user = await User.create(data);
 
     //if user details is captured
     //generate token with the user's id and the secretKey in the env file
-    // set cookie with the token generated
+    // set cookie with the token generated (optional)
     if (user) {
       let token = jwt.sign({ id: user.id }, process.env.secretKey, {
         expiresIn: 1 * 24 * 60 * 60 * 1000,
@@ -32,7 +31,7 @@ const signup = async (req, res) => {
       });
       console.log("user", JSON.stringify(user, null, 2));
       console.log(token);
-      //send users details
+
       return res.status(201).send(user);
     } else {
       return res.status(409).send("Details are not correct");
